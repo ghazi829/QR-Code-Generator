0448a6b090496e7abd59093ccf64ee98dee22b7a
ghazi 829 <khanmpir@gmail.com>
2025-11-30 20:46:04 +0500

    add the download

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
     let imgBox = document.getElementById("imgBox");
     let qrImage = document.getElementById("qrImage");
     let qrText = document.getElementById("qrText");
+    let downloadLink = document.getElementById("downloadLink");
 
 
     function generateQRCode() {
@@ -9,12 +10,15 @@
         if(qrText.value.length > 0){
             qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data= "+qrText.value;
             imgBox.classList.add("show-img");
+            downloadLink.href = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data= "+qrText.value;
+            downloadLink.style.display = "block";
         }
         else{
             qrText.classList.add("error");
             setTimeout(() => {
                 qrText.classList.remove("error");
             }, 1000);
+            downloadLink.style.display = "none";
         }
-    
+
     }
